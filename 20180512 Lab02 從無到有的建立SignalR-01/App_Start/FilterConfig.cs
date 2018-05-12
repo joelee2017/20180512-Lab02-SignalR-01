e0f7552f6f807e4b877f19bc60c2c27ce03c756d
joelee2017 <as955133@gmail.com>
2018-05-12 14:50:44 +0800

    加入專案檔案。

---
+++ 20180512 Lab02 從無到有的建立SignalR-01/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _20180512_Lab02_從無到有的建立SignalR_01
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ 20180512 Lab02 從無到有的建立SignalR-01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_20180512_Lab02_從無到有的建立SignalR_01.MvcApplication" Language="C#" %>
